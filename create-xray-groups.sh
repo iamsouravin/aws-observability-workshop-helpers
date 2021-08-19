ffc7f10f06429d4426c78346364a1a8faf068436
Sourav Paul <iamsouravin@gmail.com>
2021-08-19 11:04:48 +0530

    Add insights-configuration

--- create-xray-groups.sh
+++ create-xray-groups.sh
@@ -2,24 +2,30 @@
 
 aws xray create-group \
   --group-name Home \
-  --filter-expression "http.url ENDSWITH \"/\" AND !(http.url ENDSWITH \"/housekeeping/\" OR http.url ENDSWITH \"/PetListAdoptions\") AND http.method = \"GET\" AND service(\"PetSite\")"
+  --filter-expression "http.url ENDSWITH \"/\" AND !(http.url ENDSWITH \"/housekeeping/\" OR http.url ENDSWITH \"/PetListAdoptions\") AND http.method = \"GET\" AND service(\"PetSite\")" \
+  --insights-configuration "InsightsEnabled=true,NotificationsEnabled=true"
 
 aws xray create-group \
   --group-name PetSearch \
-  --filter-expression "http.url CONTAINS \"selectedPetType\" AND http.url CONTAINS \"selectedPetColor\" AND http.method = \"GET\" AND service(\"PetSite\")"
+  --filter-expression "http.url CONTAINS \"selectedPetType\" AND http.url CONTAINS \"selectedPetColor\" AND http.method = \"GET\" AND service(\"PetSite\")" \
+  --insights-configuration "InsightsEnabled=true,NotificationsEnabled=true"
 
 aws xray create-group \
   --group-name TakeMeHome \
-  --filter-expression "http.url ENDSWITH \"/adoption/takemehome\" AND http.method = \"POST\" AND service(\"PetSite\")"
+  --filter-expression "http.url ENDSWITH \"/adoption/takemehome\" AND http.method = \"POST\" AND service(\"PetSite\")" \
+  --insights-configuration "InsightsEnabled=true,NotificationsEnabled=true"
 
 aws xray create-group \
   --group-name MakePayment \
-  --filter-expression "http.url ENDSWITH \"/Payment/MakePayment\" AND http.method = \"POST\" AND service(\"PetSite\")"
+  --filter-expression "http.url ENDSWITH \"/Payment/MakePayment\" AND http.method = \"POST\" AND service(\"PetSite\")" \
+  --insights-configuration "InsightsEnabled=true,NotificationsEnabled=true"
 
 aws xray create-group \
   --group-name ListAdoptions \
-  --filter-expression "http.url ENDSWITH \"/PetListAdoptions\" AND http.method = \"GET\" AND service(\"PetSite\")"
+  --filter-expression "http.url ENDSWITH \"/PetListAdoptions\" AND http.method = \"GET\" AND service(\"PetSite\")" \
+  --insights-configuration "InsightsEnabled=true,NotificationsEnabled=true"
 
 aws xray create-group \
   --group-name Housekeeping \
-  --filter-expression "http.url ENDSWITH \"/housekeepin€g/\" AND http.method = \"GET\" AND service(\"PetSite\")"
+  --filter-expression "http.url ENDSWITH \"/housekeepin€g/\" AND http.method = \"GET\" AND service(\"PetSite\")" \
+  --insights-configuration "InsightsEnabled=true,NotificationsEnabled=true"
